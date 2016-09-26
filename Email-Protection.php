c1c34ac38644a1250dd2013443a7011e1c38c552
Join-GitHub-Now <anemailaddressisrequired3@gmail.com>
2016-09-26 22:22:37 +1000

    Create Email-Protection.php

---
+++ Email-Protection.php
@@ -0,0 +1,34 @@
+<?php
+
+echo obscure('hello@codecourse.com');
+
+function obscure($value)
+{
+    $obscured = null;
+    $identifier = md5(uniqid(true));
+
+    $charset = '+-.0123456789@ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz';
+    $key = str_shuffle($charset);
+
+    for ($i = 0; $i < strlen($value); $i++) {
+        $obscured .= $key[strpos($charset, $value[$i])];
+    }
+
+    $output = <<<EOT
+        <span id="{$identifier}">[email protected]</span>
+        <script>
+            (function (k, o) {
+                var c = k.split('').sort().join('');
+                var r = '';
+
+                for (var i = 0; i < o.length; i = i + 1) {
+                    r += c.charAt(k.indexOf(o.charAt(i)));
+                }
+                
+                document.getElementById('{$identifier}').innerText = r;
+            })('{$key}', '{$obscured}');
+        </script>
+EOT;
+
+    return trim(preg_replace('/\s+/', ' ', $output));
+}
