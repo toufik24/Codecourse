28275d8fa347c91f1c5bbf0b17358145e5bdf2c8
Join-GitHub-Now <anemailaddressisrequired3@gmail.com>
2016-09-27 14:44:21 +1000

    Add files via upload

---
+++ Basic-PHP-CMS/app/views/templates/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>My CMS</title>
+
+		<link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/app.css">
+	</head>
+	<body>
+		<div class="wrapper">
+			<h1>My CMS</h1>
---
+++ Basic-PHP-CMS/index.php
@@ -0,0 +1,10 @@
+<?php
+
+require 'app/start.php';
+
+$pages = $db->query("
+	SELECT id, label, slug
+	FROM pages
+")->fetchAll(PDO::FETCH_ASSOC);
+
+require VIEW_ROOT . '/home.php';
